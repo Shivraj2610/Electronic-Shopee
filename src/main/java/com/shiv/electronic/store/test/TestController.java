6d59188f4c5c37a67ed4016de30c5e07dc1b375b
Shivraj2610 <shivrajnalavade17@gmail.com>
2025-09-15 12:36:01 +0530

    Start My Electronic Shopee Backend project

---
+++ src/main/java/com/shiv/electronic/store/test/TestController.java
@@ -0,0 +1,16 @@
+package com.shiv.electronic.store.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testProject(){
+        return "Project Started";
+    }
+}
